718fa5a66ba55e8de910ccf7e41444ebc134b8de
Brydon Holsey <brydon.holsey@ufl.edu>
2020-03-28 21:54:00 -0400

    try to fix setTimeout

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 // It is always helpful to use comments in your code!
+// var tempList = (temperatures) => document.getElementById('boiling-temp').innerHTML = temperatures[0];
+// const tempList = (temperatures) => document.getElementById('freezing-temp').innerHTML = temperatures[1];
+// let tempList = (temperatures) => document.getElementById('current-temp').innerHTML = temperatures[2];
 
-var tempList = function() {
-    $('boiling-temp').innerHTML = tempList[0];
-    document.getElementById('freezing-temp').innerHTML = tempList[1];
-    document.getElementById('water-temp').innerHTML = tempList[2];
+
+var tempList = function(temperatures) {
+    document.getElementById('boiling-temp').innerHTML = temperatures[0];
+    document.getElementById('freezing-temp').innerHTML = temperatures[1];
+    document.getElementById('water-temp').innerHTML = temperatures[2];
 };
 
 tempList(["212", "32", "13"]);
+
+setTimeout(function() {
+    if (temperatures[2] >= 212) {
+        // document.getElementById('boiling').classList.remove(hide);
+        console.log('test');
+        }
+    }, 3000);
