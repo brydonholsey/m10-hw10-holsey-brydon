95866a7d8386a3cab1cc43c92cbe3da0889d4afe
Brydon Holsey <brydon.holsey@ufl.edu>
2020-03-28 20:55:40 -0400

    trying to figure out how to get homework working

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // It is always helpful to use comments in your code!
+
+var tempList = function() {
+    $('boiling-temp').innerHTML = tempList[0];
+    document.getElementById('freezing-temp').innerHTML = tempList[1];
+    document.getElementById('water-temp').innerHTML = tempList[2];
+};
+
+tempList(["212", "32", "13"]);
